18f1f0cb9b725c5a56049e600e012cf8be4f3b58
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-08-25 13:12:21 +0700

    Delete blocks directory

--- blocks/blue_gen.js
+++
@@ -1,8 +0,0 @@
-Blockly.Python['blue_con'] = function(block) {
-    
-    Blockly.Python.definitions_['import_blue_con'] = 'import blue_con';
-    
-    var code = '\n'
-    return code;
-  };
-  
--- blocks/ir_gen.js
+++
@@ -1,8 +0,0 @@
-Blockly.Python['ir_get_sig'] = function(block) {
-    
-    Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
-
-    var code = '#while True:\ncommand = beetlecar.read_ircode(beetlecar.ird)\nprint(command)\ntime.sleep(1)'
-    return code;
-  };
-  
